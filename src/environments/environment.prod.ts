4a0d61ed87c9d4d5bb39373a5d37f585377115d0
Gonzalo Espinoza <go.espinoza.a@gmail.com>
2021-03-26 23:14:16 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,36 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://go-api-ripley.herokuapp.com/api/',
+  services: {
+    register: {
+      isPublic: true,
+      isToken: false,
+      endpoint: 'public/user/register',
+      serviceName: 'register'
+    },
+    login: {
+      isPublic: true,
+      isToken: true,
+      endpoint: 'public/user/login',
+      serviceName: 'login'
+    },
+    account: {
+      isPublic: false,
+      isToken: true,
+      endpoint: 'private/account',
+      serviceName: 'account'
+    },
+    trx: {
+      isPublic: false,
+      isToken: true,
+      endpoint: 'private/trx',
+      serviceName: 'trx'
+    },
+    users: {
+      isPublic: false,
+      isToken: true,
+      endpoint: 'private/users',
+      serviceName: 'users'
+    }
+  }
 };
