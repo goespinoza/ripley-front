59a7674f49b14e9dd13372262b9f4e4acf06e27a
Gonzalo Espinoza <go.espinoza.a@gmail.com>
2021-03-27 00:02:05 -0300

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/ripley-front'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ripley-front/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/ripley-front', 'index.html'));
 });
 
 // default Heroku port
